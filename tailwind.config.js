d88d94b6448f09e8a988704f4a8eeac2e14c4105
juliokof2104 <126487919+juliokof2104@users.noreply.github.com>
2025-07-13 21:23:23 -0300

    Add Lato font alias

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Lato', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
         raleway: ['Raleway', 'sans-serif'],
       },
 
